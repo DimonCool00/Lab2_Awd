e44e91c1355fc81f2211293cfb28fac4d35ba760
DimonCool00 <smirdimon2000@mail.ru>
2021-02-12 13:06:31 +0600

    Lab2_AdvWeb

--- src/app/add-record/add-record.component.ts
+++ src/app/add-record/add-record.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgForm} from '@angular/forms';
+export class Result{
+  constructor(public date: string | undefined,
+              public distance: number | undefined,
+              public time: string | undefined)
+  { }
+}
 @Component({
   selector: 'app-add-record',
   templateUrl: './add-record.component.html',
@@ -7,9 +13,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddRecordComponent implements OnInit {
 
-  constructor() { }
+  date: string | undefined;
+  distance: number | undefined;
+  time: string | undefined ;
+  results: Result [] = [];
+  addRes(){
+    this.results.push(new Result(this.date, this.distance, this.time));
+  }
+  printResult(form: NgForm){
+    console.log(form);
+  }
 
   ngOnInit(): void {
   }
-
 }
+
